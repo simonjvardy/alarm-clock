ae0270b437b2fde0a7157b33dcbfb9837603d7c4
Simon Vardy <65361514+simonjvardy@users.noreply.github.com>
2020-11-24 21:49:24 +0000

    Update: Get the current date and build the seconds hand use Date() function to generate the current time Build the seconds hand using HTML canvas coordinates to draw lines

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -4,44 +4,59 @@ const context = canvas.getContext('2d'); // The context method contains all the 
 
 
 // Configure the loading of the background image
-// Ensure the image has loaded from the server first - clockImageLoaded returns true once the image is loaded
+// Ensure the image has loaded from the server first - clockFaceImageLoaded returns true once the image is loaded
+// code adapted from http://www.dhtmlgoodies.com/tutorials/canvas-clock/
 const clockFaceImg = new Image();
-let clockImageLoaded = false;
+let clockFaceImageLoaded = false;
 clockFaceImg.onload = function() {
-    clockImageLoaded = true;
+    clockFaceImageLoaded = true;
 }
 clockFaceImg.src = 'assets/img/clock-face.png';
 
 
 // Add the clock face
-function addBackgroundImage() {
+function loadBackgroundImage() {
     // After the context.translate() remap of (0,0) pos, the top left corner of the image is in the centre of the canvas!
     // Reposition the image back to the canvas top left corner e.g. (-200, -200, 400, 400)
     context.drawImage(clockFaceImg, canvas.width/2 * -1 ,canvas.height/2 * -1,canvas.width, canvas.height);
 }
 
-// Draw the clock hour hand
+// Create the clock hour hand
+createHourHand(currentDate) {
 
+}
 
-// Draw the clock minutes hand
+// Create the clock minutes hand
+createMinuteHand(currentDate) {
 
+}
 
-// Draw the clock seconds hand
-
+// Create the clock seconds hand
+createSecondHand(currentDate) {
+    let sec = currentDate.getSeconds();  // Gives us a value between 0-59
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(0,0);
+    context.lineTo(0,-150);
+    context.stroke();
+}
 
 // Write text on the clock face
 
 
 // Create the whole clock
 function createClock() {
-    addBackgroundImage();
-
+    loadBackgroundImage();
+    let currentDate = new Date();
+    createHourHand(currentDate);
+    createMinuteHand(currentDate);
+    createSecondHand(currentDate);
 }
 
 // Make the clock run
 function clock() {
     // Test that the clock background image file has loaded before creating the clock - if not, wait 10ms and try calling the clock() function again
-    if (!clockImageLoaded) {
+    if (!clockFaceImageLoaded) {
         setTimeout('clock()', 10);
         return;
     }    
