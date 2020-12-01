843aa38f0671c4c18c2a6f7ca252b40d3b509be1
Simon Vardy <65361514+simonjvardy@users.noreply.github.com>
2020-12-01 16:55:23 +0000

    Refactor: the alam clock functionality and set / clear button CSS: add classes to draw the button shap and set the colour / hover styling Bootstrap classes clash with setting the button css with javascript. Refactor alarm.js as it fails JSLint on line length, unexpected whitespace etc. Update README.md with new colour scheme for the gradient transitions

--- assets/js/alarm.js
+++ assets/js/alarm.js
@@ -1,48 +1,68 @@
 // Code adapted from// https://www.youtube.com/watch?v=v3kDlRx0c5M
+/*jshint esversion: 6 */
 
-const alarmHrs = document.getElementById("alarmHrs");  // Get the DOM Element for the alarm hours selection
-const alarmMins = document.getElementById("alarmMins");  // Get the DOM Element for the alarm minutes selection
-const alarmSeconds = "00";  // Set a default seconds value of 00
-const startStop =  document.getElementById("startStop");  // Get the DOM Element for the alarm set / clear button
-const alarmDisplay = document.getElementById("alarmSound");  // 
-const bellIconDiv = document.getElementById("alarmBell");  // Get the DOM Element for the div to contain the font awesome bell icons
+// Get the DOM Elements for the Alarm Clock setion
+const alarmHrs = document.getElementById("alarmHrs");
+const alarmMins = document.getElementById("alarmMins");
+const startStop = document.getElementById("startStop");
+const alarmDisplay = document.getElementById("alarmSound");
+const bellIconDiv = document.getElementById("alarmBell");
+
+// initial page load shows the bell-slash icon to show the alarm is not set
 bellIconDiv.innerHTML = '<i class="far fa-bell-slash"></i>';
+
+// Declare the alarm calculation variables
 let currentTime;
 let alarmElement;
 let alarmActive = false;
 
-// Get the current time and compare to the alarm time set by the user
+// Get the current time and compare to the alarm time set
+// by the user once per second
 function alarmTime() {
   let now = new Date();
-  currentTime = now.toLocaleTimeString();  // Return the time portion of a Date object as a string in the format "hh:mm:ss"
-  
-  if(currentTime === alarmElement) {
-    alarmDisplay.play();  // Play the alarm sound when the current time equals the alarm time
-    document.getElementById("alarmBell").className = ".bell-icon-shake";  // Assign css class to bell icon
+
+  // Return the time portion of a Date object as a string
+  // in the format "hh:mm:ss"
+  currentTime = now.toLocaleTimeString();
+  if (currentTime === alarmElement) {
+    alarmDisplay.play();
+
+    // Assign css class to bell icon to make the image shake
+    bellIconDiv.classList.add("bell-icon-shake");
   }
-  setTimeout(alarmTime, 1000);  // Call the alarmTime function once per second
+  setTimeout(alarmTime, 1000);
 }
 alarmTime();
 
-startStop.onclick = function() {
-  if(alarmActive === false) {
-    alarmHrs.disabled = true;  // Disable the alarm Hours selector when the alarm is set
-    alarmMins.disabled = true;  // Disable the alarm Minutes selector when the alarm is set
-    
+startStop.onclick = function () {
+  if (alarmActive === false) {
+
+    // Disable the alarm Hours and minutes selectors when the alarm is set
+    alarmHrs.disabled = true;
+    alarmMins.disabled = true;
     alarmElement = alarmHrs.value + ":" + alarmMins.value + ":00";
-    // this points to the parent startStop button
-    this.innerText = "Clear Alarm";  // Change the button text to Clear Alarm when clicked
-    document.getElementById("startStop").className = "btn-danger";  // Assign css class to button
+
+    // Change the colour and text of the alarm clock button
+    this.innerText = "Clear Alarm";
+    document.getElementById("startStop").className = "btn-red btn-shape";
+
+    // Show a bell icon when the alarm is set
     bellIconDiv.innerHTML = '<i class="far fa-bell"></i>';
-    alarmActive = true;  // Set the alarmActive flag to true
+
+    alarmActive = true;
   } else {
-    alarmHrs.disabled = false;  // Enable the alarm Hours selector when the alarm is not set
-    alarmMins.disabled = false;  // Enable the alarm Minutes selector when the alarm is not set
-    
+    // Enable the alarm Hours and minutes selectors when the alarm is not set
+    alarmHrs.disabled = false;
+    alarmMins.disabled = false;
     alarmDisplay.pause();
-    this.innerText = "Set Alarm";  // Change the button text to Set Alarm when clicked
-    document.getElementById("startStop").className = "btn-success";  // Assign css class to button
+    this.innerText = "Set Alarm";
+    document.getElementById("startStop").className = "btn-green btn-shape";
+
+    // Show a bell-slash icon  when the alarm is not set
     bellIconDiv.innerHTML = '<i class="far fa-bell-slash"></i>';
+
+    //  Removes the image shake css styling class
+    bellIconDiv.classList.remove("bell-icon-shake");
     alarmActive = false;
   }
-}
+};
